2e9ccb0b5fcf4459ae569eb6289bec2a2ebe5a4b
cswirl <jay.angeles@protonmail.com>
2022-12-04 22:54:18 -0500

    Configure Logging. Add and configure Ocelot - Default Environment: Local

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Logging.AddConfiguration(configuration.GetSection("Logging"));
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+// Services
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
+// Middleware Pipeline
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
