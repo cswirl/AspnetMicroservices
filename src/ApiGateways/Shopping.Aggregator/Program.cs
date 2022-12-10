4f475c87babec6bca394531fcfe7f83d92f6ea29
cswirl <jay.angeles@protonmail.com>
2022-12-09 23:33:44 -0500

    Add Services for consuming internal microservices

--- src/ApiGateways/Shopping.Aggregator/Program.cs
+++ src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,3 +1,5 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var Configuration = builder.Configuration;
+
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => 
+    c.BaseAddress = new Uri(Configuration["ApiSettings:CatalogUrl"]));
+
+builder.Services.AddHttpClient<IBasketService, BasketService>(c =>
+    c.BaseAddress = new Uri(Configuration["ApiSettings:BasketUrl"]));
+
+builder.Services.AddHttpClient<IOrderService, OrderService>(c =>
+    c.BaseAddress = new Uri(Configuration["ApiSettings:OrderingUrl"]));
 
 var app = builder.Build();
 
